19c2e35d1ec16006ccad6bdba7fdc53f256f4c44
houguozhi <guozhi.hou@huntersun.com.cn>
2016-05-17 18:00:58 +0800

    Debugging xls2txt.

--- LuaScripts_Excel/excel2txt.lua
+++ LuaScripts_Excel/excel2txt.lua
@@ -4,12 +4,13 @@ local path = lua_file_full_path:sub(1, -lua_file_full_path:reverse():find("\\"))
 print (path)
 
 ft = {
- [[D:\Users\hgz\work\Lua\LuaScripts\LuaScripts_Excel\D94364.xls]],
+ [[D94364.xls]],
 -- [[D:\Users\hgz\work\Lua\LuaScripts\LuaScripts_Excel\HS6206(A2)-D7U317(13#14#).xls]],
--- [[D:\Users\hgz\work\Lua\LuaScripts\LuaScripts_Excel\HS6206(A2)-D7U317(13#14#)的测试数据.xls]],
+-- [[D:\Users\hgz\work\Lua\LuaScripts\LuaScripts_Excel\HS6206(A2)-D7U317(13#14#)的测试数�?xls]],
 }
 
-book = excel.Open(ft[1])
-excel.SaveAsTxt(book, ft[1]:sub(1,-5)..".txt")
+
+book = excel.Open(path..ft[1])
+excel.SaveAsTxt(book, path..ft[1]:sub(1,-5)..".txt")
 excel.Close(book)
 
--- LuaScripts_Excel/xls2txt.lua
+++ LuaScripts_Excel/xls2txt.lua
@@ -4,10 +4,19 @@ local path = lua_file_full_path:sub(1, -lua_file_full_path:reverse():find("\\"))
 print (path)
 
 ft = {
- [[D:\Users\hgz\work\Lua\LuaScripts\LuaScripts_Excel\D94364.xlsx]],
+ [[D94364.xls]],
 -- [[D:\Users\hgz\work\Lua\LuaScripts\LuaScripts_Excel\HS6206(A2)-D7U317(13#14#).xls]],
 -- [[D:\Users\hgz\work\Lua\LuaScripts\LuaScripts_Excel\HS6206(A2)-D7U317(13#14#)的测试数据.xls]],
 }
 
-excel.DoConvert(ft[1])
+function xls2txt (...)
+	local srcfile, desdir = ...
+	if (not desdir) then
+		desdir = ""
+	end
+	excel.xls2txt(srcfile, desdir);
+end
+
+-- xls2txt(path..ft[1])
+ xls2txt(path..ft[1], path..ft[1]:sub(1,-6).."\\")
 
